94306584adfeed522d3ae89401c608881885f7f1
jhay-dadzie <josephdadzie0002@gmail.com>
2026-05-11 19:27:00 +0000

    Add theme constants for light and dark color schemes

---
+++ frontend/constants/theme.ts
@@ -0,0 +1,21 @@
+import { Platform } from "react-native";
+
+const tintColorLight = '#004AC6';
+const tintColorDark = '#2563EB';
+
+export const Colors = {
+    light: {
+        text: '#6B7280',
+        background: '#ECEEFE',
+        tint: tintColorLight,
+        icon: '#9CA3AF',
+        tabBarActiveIcon: tintColorLight
+    },
+    dark: {
+        text: '#141A2F',
+        background: '#ECEEFE',
+        tint: tintColorDark,
+        icon: '#C3C6D7',
+        tabBarActiveIcon: '#B4C5FF'
+    }
+}
