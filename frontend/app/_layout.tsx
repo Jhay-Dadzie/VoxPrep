77691b931774b303f894e727141a7171de6ce149
jhay-dadzie <josephdadzie0002@gmail.com>
2026-05-12 02:34:40 +0000

    Implement RootLayout with theme support and redirect to onboarding

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,5 +1,21 @@
+import { DarkTheme, ThemeProvider, DefaultTheme } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import 'react-native-reanimated';
+import { useColorScheme } from "@/hooks/use-color-scheme"
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme()
+  return (
+    <SafeAreaProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{headerShown: false}}>
+          <Stack.Screen name="onboarding" />
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+        <StatusBar style="auto"/>
+      </ThemeProvider>
+    </SafeAreaProvider>
+  )
 }
